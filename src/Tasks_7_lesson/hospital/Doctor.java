f2fe1bce090318688ed6a6b2ca37edda10193fa8
Kseniya Mironova <137803771+Kseniya-Mi@users.noreply.github.com>
2024-08-26 10:17:48 +0300

    Update and rename Hospital.java to Doctor.java

---
+++ src/Tasks_7_lesson/hospital/Doctor.java
@@ -0,0 +1,15 @@
+package Tasks_7_lesson.hospital;
+
+public class Doctor {
+    private String name;
+
+    Doctor(){}
+
+    Doctor(String name){
+        this.doctor = name;
+    }
+
+    void treat(){
+        System.out.println("Do nothing");
+    }
+}
